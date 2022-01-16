9f4f3136d0706242a3ff365b018ff46d95215bea
Victor Crispin <victorrcrispinjr@gmail.com>
2022-01-15 19:01:59 -0500

    add initial create submission screen

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
+import dotenv from 'dotenv';
 import { useEffect, useMemo, useState } from 'react';
 import { useWeb3 } from '@3rdweb/hooks';
 import { ThirdwebSDK } from '@3rdweb/sdk';
 import { UnsupportedChainIdError } from "@web3-react/core";
 import { ethers } from 'ethers';
-import { Grid, Card, Image, Spacer } from '@geist-ui/react';
+import { Grid, Card, Image, Spacer, Text } from '@geist-ui/react';
 import ConnectWallet from './components/ConnectWallet';
-import dotenv from 'dotenv';
+import CreateSubmission from './components/CreateSubmission';
 
-dotenv.config()
+// const process = dotenv.config()
+// console.log(process.env)
 
 const sdk = new ThirdwebSDK('rinkeby'); //* Needs to change to the Polygon network
 
-const bundleDropModule = sdk.getBundleDropModule(process.env.BUNDLE_DROP_MODULE);
-const tokenModule = sdk.getTokenModule(process.env.TOKEN_MODULE);
-const voteModule = sdk.getVoteModule(process.env.VOTE_MODULE);
+// const bundleDropModule = sdk.getBundleDropModule(process.env.BUNDLE_DROP_MODULE);
+// const tokenModule = sdk.getTokenModule(process.env.TOKEN_MODULE);
+// const voteModule = sdk.getVoteModule(process.env.VOTE_MODULE);
 
 const App = () => {
   const { connectWallet, address, error, provider } = useWeb3();
@@ -27,13 +29,20 @@ const App = () => {
 
   if (error instanceof UnsupportedChainIdError ) {
     return (
-      <div className="unsupported-network">
-        <h2>Please connect to Polygon Mumbai Testnet</h2>
-        <p>
-          This dapp only works on the Polygon testnet network, please switch networks
-          in your connected wallet.
-        </p>
-      </div>
+      <Grid.Container gap={2} justify="center">
+        <Spacer h={4}/>
+        <Grid xs={24}>
+          <Image width="540px" height="160px" src="/Crypt0GrafflLogo.png" />
+        </Grid>
+        <Grid xs={24} direction="column"
+        alignItems='center'>
+          <Text h2>Please connect to Polygon Mumbai Testnet</Text>
+          <Text p>
+            This dapp only works on the Polygon testnet network, please switch networks
+            in your connected wallet.
+          </Text>
+        </Grid>
+      </Grid.Container>
     );
   }
 
@@ -48,9 +57,7 @@ const App = () => {
           connectWallet={connectWallet}
         />
       :
-        <div>
-          Replace with other stages of the app
-        </div>
+        <CreateSubmission />
       }
     </Grid.Container>
   )
--- src/components/ConnectWallet.js
+++ src/components/ConnectWallet.js
@@ -1,15 +1,19 @@
-import { Button, Text, Grid } from '@geist-ui/react'
+import { Button, Text, Grid, Spacer } from '@geist-ui/react'
 
 const ConnectWallet = (props) => {
   return (
     <Grid.Container justify="center" gap={2} className="connect-wallet-container" height="100px">
+      <Spacer h={10} />
       <Grid
         className="connect-wallet" 
         justify="center"
         direction="column"
         alignItems='center'
+        xs={16}
+        l={6}
       >
-        <Text width="80%" h2>Let's Get Started, Connect Your Wallet</Text>
+        <Text width="100%" h2>Let's Get Started, Connect Your Wallet</Text>
+        <Spacer h={5} />
         <Button
           type="secondary"
           onClick={() => props.connectWallet("injected")}
